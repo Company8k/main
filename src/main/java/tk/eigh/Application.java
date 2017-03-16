d35aafcf131301d06352d13fb6c6bfa0a07dc28e
NT <thent87@eigh.tk>
2017-03-16 19:24:52 +0000

    initial

---
+++ src/main/java/tk/eigh/Application.java
@@ -0,0 +1,18 @@
+package tk.eigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class Application {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
